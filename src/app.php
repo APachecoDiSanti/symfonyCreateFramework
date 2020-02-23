60480f9d7e914ed3a2df4e1fc82934265464fd98
APachecoDiSanti <pachecodisanti@outlook.com>
2020-02-23 18:43:43 -0300

    Templating

--- src/app.php
+++ src/app.php
@@ -1,11 +1,36 @@
 <?php
 
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
-$routes = new RouteCollection();
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+    
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
 
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Route('/bye'));
+$routes = new Routing\RouteCollection();
+
+$routes->add('hello', new Routing\Route('/hello/{name}', [
+    'name' => 'World',
+    '_controller' => 'render_template',
+]));
+
+$routes->add('bye', new Routing\Route('/bye', [
+    '_controller' => 'render_template',
+]));
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+        
+        return new Response('Nope, this is not a leap year.');
+    }
+]));
 
 return $routes;
