05cdc2980604d567723a677e428b482634ee2a63
APachecoDiSanti <pachecodisanti@outlook.com>
2020-03-21 14:56:21 -0300

    The DependencyInjection Component

--- web/front.php
+++ web/front.php
@@ -1,49 +1,36 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\ErrorHandler\Exception\FlattenException;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\EventListener\ErrorListener;
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\Routing;
+use Simplex\StringResponseListener;
+
+function render_template(Request $request) {
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+    return new Response(ob_get_clean());
+}
+
+$routes = include __DIR__.'/../src/app.php';
+$container = include __DIR__.'/../src/container.php';
+
+$container->setParameter('debug', true);
+$container->setParameter('charset', 'UTF-8');
+$container->setParameter('routes', $routes);
+
+$container->register('listener.string_response', StringResponseListener::class);
+
+$container->getDefinition('dispatcher')
+    ->addMethodCall(
+        'addSubscriber', 
+        [
+            new Reference('listener.string_response')
+        ]
+);
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
-$routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$response = $container->get('framework')->handle($request);
 
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$errorHandler = function (FlattenException $exception) {
-    $msg = 'Something went wrong! ('.$exception->getMessage().')';
-    return new Response($msg, $exception->getStatusCode());
-};
-
-$listener = new ErrorListener(
-    'Calendar\Controller\ErrorController::exception'
-);
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new ErrorListener($errorHandler));
-$dispatcher->addSubscriber(new ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-$dispatcher->addSubscriber($listener);
-
-$framework = new Simplex\Framework(
-    $dispatcher,
-    $controllerResolver,
-    $requestStack,
-    $argumentResolver
-);
-
-$framework->handle($request)->send();
+$response->send();
